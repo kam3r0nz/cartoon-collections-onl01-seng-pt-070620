4e3e5e36804d317c88fe0002c3f431c1b5535cbe
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-15 22:17:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves(dwarf)
   dwarf.each.with_index(1) do |name, num|
   dwarf << "#{num}. #{name}"
 end
+end
 
 def summon_captain_planet# code an argument here
   # Your code here
