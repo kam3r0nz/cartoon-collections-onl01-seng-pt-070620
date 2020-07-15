e53d1ea271a0fed6da201f3fe5d9eee9b4201ded
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-15 22:48:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,9 @@ end
 
 long_planeteer_calls(short_words)
 
-def find_the_cheese# code an argument here
+snacks = ["crackers", "gouda", "thyme"]
+
+def find_the_cheese(# code an argument here)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
