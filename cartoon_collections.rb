5ed68db5db3568c21e76185197feb7a079d967aa
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-15 22:45:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,10 @@ summon_captain_planet(planeteer_calls)
 short_words = ["puff", "go", "two"]
 
 def long_planeteer_calls(array)
-  
+  array.any? {|call| call.length == 4}
 end
+
+long_planeteer_calls(short_words)
 
 def find_the_cheese# code an argument here
   # the array below is here to help
