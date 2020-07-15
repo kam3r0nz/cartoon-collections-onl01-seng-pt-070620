5cb81d412e04387d1e3b017408e6aef2d4d1c3ac
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-15 22:34:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ roll_call_dwarves(dwarves)
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
 def summon_captain_planet(array)
-  planeteer_calls.collect {}
+  planeteer_calls.collect {|element| element.upcase + "!"}
 end
 
 def long_planeteer_calls# code an argument here
