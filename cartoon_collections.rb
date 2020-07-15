7ef54d9cbd803cc9870ea76a44d380cd64849814
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-15 22:29:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
 def roll_call_dwarves(array)
   i = 0
-  array.each.with_index(1) {|name, num| puts "#{num}. #{name}"
+  array.each.with_index(1) {|name, num| puts "#{num}. #{name}"}
 end
   roll_call_dwarves(dwarves)
 end
