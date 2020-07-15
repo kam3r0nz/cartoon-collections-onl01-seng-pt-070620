b16c9f7c50bd6bcbee5ae094089f3adab61edbd4
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-15 22:34:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,8 @@ def summon_captain_planet(array)
   planeteer_calls.collect {|element| element.upcase + "!"}
 end
 
+summon_captain_planet(planeteer_calls)
+
 def long_planeteer_calls# code an argument here
   # Your code here
 end
